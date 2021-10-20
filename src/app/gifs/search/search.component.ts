84e612aba3b742f701c5097488f2dcf39c046ee2
Hugo Aguero Arrieta <huguitoaguero@gmail.com>
2021-10-20 05:45:27 -0300

    use of ViewChild

--- src/app/gifs/search/search.component.ts
+++ src/app/gifs/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -6,11 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
 
-  constructor() { }
+  @ViewChild('txtSearch') txtSearch!:ElementRef<HTMLInputElement>;
 
-  ngOnInit(): void {
+  search() {
+    const search = this.txtSearch.nativeElement.value;
+    console.log(search);
+    this.txtSearch.nativeElement.value = '';
   }
 
 }
