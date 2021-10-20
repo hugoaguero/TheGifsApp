18aae7167a06ae1f1b5a660f5e43934e65244a31
Hugo Aguero Arrieta <huguitoaguero@gmail.com>
2021-10-20 04:56:24 -0300

    create: Gif module and Search, Results components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
@@ -11,7 +12,8 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
-    SharedModule
+    SharedModule,
+    GifsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
